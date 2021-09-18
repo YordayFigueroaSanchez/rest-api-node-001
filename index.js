181cb7872e026ecaf845f02f2e54bc645be10043
yfsanchez <yfsanchez@gmail.com>
2021-09-18 12:12:25 -0300

    ejecutar peticiones GET y POST
    ejecutar peticiones GET y POST

--- index.js
+++ index.js
@@ -6,9 +6,33 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
-app.get('/doble', (req, res) => {
-    res.send('Doble')
+app.get('/doble/:num', (req, res) => {
+    console.log(req.params);
+    const doble = {
+      "num" : req.params.num,
+      "valor": req.params.num * 2
+    }
+    res.json(doble);
   });
+
+app.get('/estudiantes', (req, res) => {
+  const estudiantes = [
+    {
+      "nombre":"nombre01",
+      "apellido":"apellido01"
+    },{
+      "nombre":"nombre02",
+      "apellido":"apellido02"
+    },
+]
+res.json(estudiantes);
+});
+
+app.post('/estudiantes', (req, res) => {
+  
+res.json("Datos recibidos");
+
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
