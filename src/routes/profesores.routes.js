31ecc155c227c3f60bfb433ca5b3931d6a7f7dcf
yfsanchez <yfsanchez@gmail.com>
2021-09-25 14:36:59 -0300

    agregar profesor routes
    agregar profesor routes

---
+++ src/routes/profesores.routes.js
@@ -0,0 +1,5 @@
+const { Router } = require("express");
+const controllerProfesor = require('../controllers/profesores.controller');
+const routerProfesores = Router();
+
+  routerProfesores.post("/registrar", controllerProfesor.set);
